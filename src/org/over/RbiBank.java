89b107744080a2ff6b5bf02ef3d8e9b837c16d15
7042vs <vinishvini75@gmail.com>
2023-02-04 21:03:11 +0530

    over

---
+++ src/org/over/RbiBank.java
@@ -0,0 +1,13 @@
+package org.over;
+
+public class RbiBank {
+	
+	public void Savings() {
+
+		
+		System.out.println("savings % is :2 ");
+	}
+public void Fixed() {
+System.out.println("Fixed % is :10");
+}
+}
---
+++ src/org/over/SbiBank.java
@@ -0,0 +1,35 @@
+package org.over;
+
+public class SbiBank extends RbiBank {
+	
+public void Savings() {
+ 
+	
+	System.out.println("Sbi savings % is : 7" );
+	
+	
+	
+}
+	
+	public static void main(String[] args) {
+		
+		SbiBank s = new SbiBank ();
+		
+		s.Savings();
+	
+		s.Fixed();
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
